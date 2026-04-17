8409e1092ebca4016e171d6f10de15920faaed03
Deepak Raghav <ankitraghav@Deepaks-MacBook-Pro.local>
2026-04-17 16:00:43 +0530

    Split toolsProductInfo3 into 3 and 4 - keep files under 800KB

--- data/toolsProductInfo.ts
+++ data/toolsProductInfo.ts
@@ -1,11 +1,11 @@
 import { toolsProductInfo1 } from "./toolsProductInfo1";
 import { toolsProductInfo2 } from "./toolsProductInfo2";
 import { toolsProductInfo3 } from "./toolsProductInfo3";
+import { toolsProductInfo4 } from "./toolsProductInfo4";
 
-// When any file approaches 800KB, create toolsProductInfo4.ts
-// and add it to the spread below
 export const toolsProductInfoData: { [key: string]: any } = {
   ...toolsProductInfo1,
   ...toolsProductInfo2,
   ...toolsProductInfo3,
+  ...toolsProductInfo4,
 };
