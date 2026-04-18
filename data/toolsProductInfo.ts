9ff20325bdf0158fdcbdbec95b511d16a69497f2
Deepak Raghav <ankitraghav@Deepaks-MBP.Excitel>
2026-04-18 22:01:38 +0530

    Split toolsProductInfo4 into 4 and 5 - keep files under 800KB

--- data/toolsProductInfo.ts
+++ data/toolsProductInfo.ts
@@ -2,10 +2,12 @@ import { toolsProductInfo1 } from "./toolsProductInfo1";
 import { toolsProductInfo2 } from "./toolsProductInfo2";
 import { toolsProductInfo3 } from "./toolsProductInfo3";
 import { toolsProductInfo4 } from "./toolsProductInfo4";
+import { toolsProductInfo5 } from "./toolsProductInfo5";
 
 export const toolsProductInfoData: { [key: string]: any } = {
   ...toolsProductInfo1,
   ...toolsProductInfo2,
   ...toolsProductInfo3,
   ...toolsProductInfo4,
+  ...toolsProductInfo5,
 };
